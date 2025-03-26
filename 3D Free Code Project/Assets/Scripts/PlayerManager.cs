e25668145128844477b4b325dfc430a9fc5141bf
Kaden Nielsen <tadernielsen@gmail.com>
2025-03-25 20:05:16 -0600

    Created simple sanity changer

--- 3D Free Code Project/Assets/Scripts/PlayerManager.cs
+++ 3D Free Code Project/Assets/Scripts/PlayerManager.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    // Player Data Members
     private float sanityChanger;
+    private float sanityChangeInterval;
 
     public float playerSanity;
     public string playerState;
     
     private GameObject playerCamera;
 
+    // Other Members
+    float time;
 
+    private void UpdateSanity()
+    {
+        playerSanity += sanityChanger;
+        
+        if (playerSanity < 0)
+        {
+            playerSanity = 0;
+        }
+        else if (playerSanity > 100)
+        {
+            playerSanity = 100;
+        }
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         playerState = "Normal";
-        playerSanity = 75;
-        sanityChanger = 0;
+        playerSanity = 75f;
+        sanityChanger = 0f;
+        sanityChangeInterval = 1;
 
         playerCamera = GameObject.Find("Main Camera");
     }
@@ -24,6 +42,11 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+        if (time >= sanityChangeInterval)
+        {
+            UpdateSanity();
+            time -= sanityChangeInterval;
+        }
     }
 }
