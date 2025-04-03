5d5c32ff740cb67c3eb4c20258693798b8c85e11
Kaden Nielsen <tadernielsen@gmail.com>
2025-04-02 19:11:20 -0600

    Removed Uneeded Code

--- 3D Free Code Project/Assets/Scripts/HoldingObject.cs
+++ 3D Free Code Project/Assets/Scripts/HoldingObject.cs
@@ -3,33 +3,24 @@ using UnityEngine;
 public class HoldingObject : MonoBehaviour
 {
     public string objectName;
-    public string objectType;
 
     public bool isActive;
 
     public int calmLevel;
     public int fuel;
-    public float duration;
 
     public void useObject()
     {
-        if (objectType == "fuel")
+        if (fuel > 0)
         {
-            if (fuel > 0)
-            {
-                isActive = true;
-                fuel -= 1;
-                Debug.Log("Fuel: " + fuel);
-            }
-            else
-            {
-                isActive = false;
-                Debug.Log("No fuel left!");
-            }
+            isActive = true;
+            fuel -= 1;
+            Debug.Log("Fuel: " + fuel);
         }
-        else if (objectType == "duration")
+        else
         {
-
+            isActive = false;
+            Debug.Log("No fuel left!");
         }
     }
 }
