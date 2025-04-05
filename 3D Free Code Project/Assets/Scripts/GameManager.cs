6f55bdfb47bd0c5ab90dc16acc905b3e0da69154
Kaden Nielsen <tadernielsen@gmail.com>
2025-04-04 18:36:20 -0600

    Added Game Manager

---
+++ 3D Free Code Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private TextMeshPro sanityText;
+    private PlayerManager playerManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        sanityText = GameObject.Find("SanityCounter").GetComponent<TextMeshPro>();
+
+        playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentSanity = playerManager.playerSanity;
+
+        string sanityTextValue = "SANITY: " + currentSanity.ToString();
+        
+        sanityText.text = sanityTextValue;
+    }
+}
