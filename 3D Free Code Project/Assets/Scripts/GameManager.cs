8babc0635609e3aac9502ccd66806359276d1e9f
Kaden Nielsen <tadernielsen@gmail.com>
2025-04-09 19:24:42 -0600

    Tried Out Post Processing

--- 3D Free Code Project/Assets/Scripts/GameManager.cs
+++ 3D Free Code Project/Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
     private TextMeshProUGUI sanityText;
     private PlayerManager playerManager;
+
+    private Volume volume;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,21 +16,26 @@ public class GameManager : MonoBehaviour
         sanityText = GameObject.Find("SanityCounter").GetComponent<TextMeshProUGUI>();
 
         playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+
+        volume = GameObject.Find("Global Volume").GetComponent<Volume>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        volume.profile.TryGet(out Vignette viginette);
         float currentSanity = playerManager.playerSanity;
         string sanityOr;
 
         if (currentSanity > 20)
         {
             sanityOr = "SANITY: ";
+            viginette.active = false;
         }
         else
         {
             sanityOr = "INSANITY: ";
+            viginette.active = true;
         }
 
         string sanityTextValue = sanityOr + currentSanity.ToString();
