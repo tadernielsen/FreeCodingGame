80107fbf80d6086e8b0cd9056a91aae6253a6b82
Kaden Nielsen <tadernielsen@gmail.com>
2025-04-10 15:41:28 -0600

    Added PickUp Script

---
+++ 3D Free Code Project/Assets/Scripts/PickUpObject.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    public String objectName;
+    public bool isGrabbable;
+    public int fuel;
+    public int weight;
+}
